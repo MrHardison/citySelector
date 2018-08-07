f3c9b1da49ed356f285ffe5bc7cc2c480da0703d
Alec <disscovery.993@gmail.com>
2018-08-07 22:49:50 +0300

    upd

--- src/CitySelector/index.js
+++ src/CitySelector/index.js
@@ -1,2 +1,28 @@
 require('./style.less');
 // Your code...
+
+class CitySelector {
+	constructor(elementId, regionsUrl, localitiesUrl, saveUrl) {
+		var newId = '';
+		var newLocalUrl = '';
+		var newRegUrl = '';
+		var newSaveUrl = '';
+
+		this.setElementid = function(elementId) {
+			newId = elementId;
+		};
+		this.setRegionsurl = function(regionsUrl) {
+			newLocalUrl = regionsUrl;
+		};
+		this.setLocalitiesurl = function(localitiesUrl) {
+			newRegUrl = localitiesUrl;
+		};
+		this.setSaveurl = function(saveUrl) {
+			newSaveUrl = saveUrl;
+		};
+		this.getId = function() {
+			return newId;
+		};
+	}
+	
+};
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const CitySelector = require('./CitySelector');
+const CitySelector = import('./CitySelector');
 
-/* Пример создания компонента:
-const citySelector = new CitySelector({
-    elementId: 'citySelector',
-    regionsUrl: 'http://localhost:3000/regions',
-    localitiesUrl: 'http://localhost:3000/localities',
-    saveUrl: 'http://localhost:3000/selectedRegions'
-});
-*/
+
+// Пример создания компонента:
+// const citySelector = new CitySelector({
+	// 	elementId: 'citySelector',
+	// 	regionsUrl: 'http://localhost:3000/regions',
+	// 	localitiesUrl: 'http://localhost:3000/localities',
+	// 	saveUrl: 'http://localhost:3000/selectedRegions'
+	// });
+var selector = new CitySelector('citySelector','http://localhost:3000/regions','http://localhost:3000/localities','http://localhost:3000/selectedRegions');
